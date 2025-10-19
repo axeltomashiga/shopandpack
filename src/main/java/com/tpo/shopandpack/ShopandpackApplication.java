8d5ca73ff4af75f6c242de2aae54571016d5e031
axeltomashiga <axeltomashiga@gmail.com>
2025-10-19 19:28:54 +0100

    first commit

---
+++ src/main/java/com/tpo/shopandpack/ShopandpackApplication.java
@@ -0,0 +1,13 @@
+package com.tpo.shopandpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopandpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopandpackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpo/shopandpack/ShopandpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpo.shopandpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopandpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
